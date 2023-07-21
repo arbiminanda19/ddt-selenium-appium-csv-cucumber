ed1a2c770344698d31d6bf16ea19ea0b023e998d
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-07-21 16:06:10 +0700

    Interact with Element to Success Login

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,5 +1,6 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,6 +19,10 @@ public class Login {
         driver = new ChromeDriver(opt); // apply chrome driver setup to driver
         driver.manage().window().maximize(); // maximize window
         driver.get(baseUrl); // access base url
+
+        driver.findElement(By.id("email")).sendKeys("tdd-selenium@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("tdd-selenium");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
 
 }
